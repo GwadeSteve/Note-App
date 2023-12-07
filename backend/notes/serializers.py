ca4c916e1dba5a93b9e852ce6e054ee9cc6f2504
GSteve12 <gwadesteve12@gmail.com>
2023-12-07 16:24:49 +0100

    All CRUD endpoints are functional

---
+++ backend/notes/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user'] = user  # Assign the user directly to validated data
+        return super().create(validated_data)
