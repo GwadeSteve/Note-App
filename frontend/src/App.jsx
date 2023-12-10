327d06d3f9602ca96d06f5c16d36607415198777
GSteve12 <gwadesteve12@gmail.com>
2023-12-10 21:42:10 +0100

    SetUp frontend and api call to check user_ath_status and redirect accordingly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoadingPage from './pages/LoadingPage';
+import LoginPage from './pages/LoginPage';
+import NotesList from './pages/NotesList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoadingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notes" element={<NotesList />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import RandomNote from '../components/RandomNote';
+
+const LoginPage = () => {
+
+    const Quotes = [
+        {
+          title: "Stars in the Night",
+          quote: "Illuminate your thoughts like stars on a dark canvas; let your notes be the constellations of your journey, guiding you through the depths of creativity and imagination."
+        },
+        {
+          title: "Inscribed Echoes",
+          quote: "Inscribe your moments with ink, for within those notes lie the echoes of your dreams waiting to be unveiled—a symphony of memories yearning to fill the pages of your story."
+        },
+        {
+          title: "Pages of Legacy",
+          quote: "Every note holds a story, every thought a chapter; pen your narrative on the blank canvas of each page, crafting a legacy that speaks of your unique perspective and wisdom."
+        },
+        {
+          title: "Melodies in Ink",
+          quote: "Your notes, like melodies on a page, compose the symphony of your life's narrative—a beautiful medley of thoughts and emotions waiting to be written and sung."
+        },
+        {
+          title: "Visionary Chronicles",
+          quote: "Write your ideas, sketch your visions; in the pages of notes, you'll find the roadmap to your aspirations—a chronicle of ambitions waiting to transform into tangible realities."
+        }
+      ];
+        
+  return (
+    
+    <div>
+      <h1>We are on login</h1>
+      <RandomNote quotes={Quotes} />
+    </div>
+  )
+}
+
+export default LoginPage;
